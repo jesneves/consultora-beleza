d62bd48ed46df5bd1bb945b800616117980be6b5
Jessica Neves <jesneves@gmail.com>
2021-07-18 23:43:05 -0300

    add landing page - hiring coders / VTEX

---
+++ landing-page/js/script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // alert('E-mail cadastrado com sucesso!')
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        name,
+        email,
+    }
+
+    let convertData = JSON.stringify(data);
+    localStorage.setItem('lead', convertData)
+
+    let content = document.getElementById('content');
+
+    let carregando = '<img width="50px" src="../assets/loading.gif" alt="Logotipo">';
+    let pronto = '<p>Cadastrado com sucesso!<p>'
+// '   <a href="index.html"><button id="button">Ir para home</button></a>';
+
+    content.innerHTML = carregando;
+
+    setTimeout(() => {
+        content.innerHTML = pronto
+    }, 1000)
+
+})
