7a3facf43d9f575fe565444bd3348a9ed1075f32
Jessica Neves <jesneves@gmail.com>
2021-07-25 00:26:44 -0300

    alterando versão mobile web

--- js/scripts.js
+++ js/scripts.js
@@ -81,7 +81,6 @@ $(function () {
   var imgIterator = '';
   var imgIterator2 = '';
 
-
   if (windowWidth >= 611 && windowWidth <= 800) {
     numberOfImg = 9;
   }
@@ -98,8 +97,6 @@ $(function () {
     numberOfImg = 3;
   }
 
-
-
   for (let i = numberOfImg; i > 0; i--) {
     imgIterator = '<div class="img-promo-' + i +'"> <a href=""><img id="' + i +'" src="../img/promotion' + i +'.jpg" alt="' + i +' imagem de produto na promoção"/></a></div>' + imgIterator2;
     imgIterator2 = imgIterator;
@@ -108,32 +105,76 @@ $(function () {
   imgPromotions.innerHTML = imgIterator;
 
   // document.location.reload();
+
+  let withoutLogo = document.getElementById("id-logo");
+  let menu = document.getElementById("menu");
+  if (windowWidth < 600) {
+    withoutLogo.innerHTML;
+    menu.innerHTML = '<nav class="nav-home"><div class="menu-drop">'+
+    '<div class="btn-menu">'+
+      '<a type="button" id="menu-btn" name="drop">'+
+        '<i class="fas fa-bars" aria-hidden="true"></i>'+
+      '</a>'+
+    '</div>'+
+    '<div class="drop-content">'+
+  '<a href="#">Home</a>'+
+  '<a href="../html/tips.html">Dicas</a>'+
+  '<a href="../html/promotions.html">Promoções</a>'+
+  '<a href="../html/contact.html">Contato</a>'+
+  '<a href="../html/about.html">Sobre Nós</a>'+
+    '</div>'+
+  '</div></nav>';
+
+  } else {
+    withoutLogo.innerHTML = '<a href="/index.html"><img src="./img/logo-consultora.jpg" alt="logo da página" /></a>';
+  //   menu.innerHTML = ' <nav class="nav-home">'+
+  //   '<a href="#">Home</a>'+
+  // '<a href="../html/tips.html">Dicas da Jess</a>'+
+  // '<a href="../html/promotions.html">Promoções</a>'+
+  // '<a href="../html/contact.html">Contato</a>'+
+  // '<a href="../html/about.html">Sobre Nós</a>'+
+  // '</nav>';
+  
+    // menu.innerHTML =   '<!-- Dropdown Trigger -->'+
+    // '<a class="dropdown-trigger btn" href="#" data-target="dropdown1">Drop Me!</a>'+
+  
+    // '<!-- Dropdown Structure -->'+
+    // '<ul id="dropdown1" class="dropdown-content">'+
+    //   '<li><a href="#!">one</a></li>'+
+    //   '<li><a href="#!">two</a></li>'+
+    //   '<li class="divider" tabindex="-1"></li>'+
+    //   '<li><a href="#!">three</a></li>'+
+    //   '<li><a href="#!"><i class="material-icons">view_module</i>four</a></li>'+
+    //   '<li><a href="#!"><i class="material-icons">cloud</i>five</a></li>'+
+    // '</ul>';
+
+
+    function GetCards() {
+      let showCards = document.getElementById("promotionCards");
+      let arrayOfHtml = [];
+      // showCards.innerHTML = '<div class="buttons-line-1">';
+    
+      for (let i = 1; i < 8; i++) {
+        arrayOfHtml.push('<div class="promo-card" id="' + i + '">'+
+          '<div class="img-page-promo">'+
+              '<a href="#"><img src="../img/promotion' + i +'.jpg" alt="">'+
+              '<h6>Deo Parfum Luna Intenso</h6></a>'+
+              '</div>'+
+        '<p>'+
+          'Compre diretamente do site da <strong>Avon</strong> e receba na'+
+          'comodidade de sua casa.'+
+        '</p>'+
+        '<nav><a target="_blank" href="https://www.jesneves.loja.avon.com.br" class="waves-effect waves-light btn" >VER PROMOÇÃO</a></nav>'+
+      '</div>');
+      }
+      // showCards.innerHTML = '</div>'
+    
+      showCards.innerHTML = arrayOfHtml;
+      
+      console.log("Estoy aquí");
+    }
+  }
 });
 
-function GetCards() {
-  let showCards = document.getElementById("promotionCards");
-  let arrayOfHtml = [];
-  // showCards.innerHTML = '<div class="buttons-line-1">';
 
-  for (let i = 1; i < 4; i++) {
-    arrayOfHtml.push('<div class="promo-card" id="' + i + '">'+
-      '<div class="img-page-promo">'+
-          '<a href="#"><img src="../img/promotion' + i +'.jpg" alt="">'+
-          '<h6>Deo Parfum Luna Intenso</h6></a>'+
-          '</div>'+
-    '<p>'+
-      'Compre diretamente do site da <strong>Avon</strong> e receba na'+
-      'comodidade de sua casa.'+
-    '</p>'+
-    '<nav><a target="_blank" href="https://www.jesneves.loja.avon.com.br" class="waves-effect waves-light btn" >VER PROMOÇÃO</a></nav>'+
-  '</div>');
-  }
-  // showCards.innerHTML = '</div>'
-
-  showCards.innerHTML = arrayOfHtml;
-  
-  console.log("Estoy aquí");
-}
-
-// imgPromotions.innerHTML = '<div class="img-promo-' + i +'"> <a href=""><img id="' + i +'" src="./img/promotion' + i +'.jpg" alt="' + i +' imagem de produto na promoção"/></a></div>';
 
